1f5d48ff506b1dc8bd75b654d35816c3f5c8399d
Gniot <orzesekyt@gmail.com>
2024-10-02 16:57:40 +0200

    Update and rename js_language to js_language.js

---
+++ javascript/js_language.js
@@ -0,0 +1,40 @@
+const translations = {
+    pl: {
+        homeTitle: "Strona Główna",
+        homeContent: "Witaj na stronie głównej!",
+        settingsTitle: "Ustawienia",
+        themeLabel: "Wybierz motyw:",
+        languageLabel: "Wybierz język:",
+        lightTheme: "Jasny",
+        darkTheme: "Ciemny",
+        polishLanguage: "Polski",
+        englishLanguage: "Angielski",
+        about: "O nas",
+        gallery: "Galeria",
+        contact: "Kontakt",
+        login: "Zaloguj się", // Dodane tłumaczenie
+        register: "Zarejestruj się", // Dodane tłumaczenie
+        usernamePlaceholder: "Nazwa użytkownika", // Placeholder dla nazwy użytkownika
+        passwordPlaceholder: "Hasło", // Placeholder dla hasła
+
+    },
+    en: {
+        homeTitle: "Home Page",
+        homeContent: "Welcome to the home page!",
+        settingsTitle: "Settings",
+        themeLabel: "Select theme:",
+        languageLabel: "Select language:",
+        lightTheme: "Light",
+        darkTheme: "Dark",
+        polishLanguage: "Polish",
+        englishLanguage: "English",
+        about: "About Us",
+        gallery: "Gallery",
+        contact: "Contact",
+        login: "Log In", // Dodane tłumaczenie
+        register: "Register", // Dodane tłumaczenie
+        usernamePlaceholder: "Username", // Placeholder dla nazwy użytkownika
+        passwordPlaceholder: "Password", // Placeholder dla hasła
+    }
+};
+
