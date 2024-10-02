dc4078eb9e12a54cb0e6509283e02f05dd5919e5
Okruch165 <oskarjablonski272@gmail.com>
2024-10-02 17:13:53 +0200

    Update js_menu.js

--- javascript/js_menu.js
+++ javascript/js_menu.js
@@ -1,44 +1,7 @@
-function setDefaultTheme() {
-    const savedTheme = localStorage.getItem('theme') || 'dark';
-    document.body.classList.add(`${savedTheme}-theme`);
-}
-
-// Funkcja do renderowania treści na stronie głównej
-function renderHomePage(lang) {
-    const tresc = document.getElementById('tresc');
-    tresc.innerHTML = `
-        <h2>${translations[lang].homeTitle}</h2>
-        <p>${translations[lang].homeContent}</p>
-    `;
-}
-
-// Funkcja do renderowania ustawień
-function renderSettings(lang) {
-    const tresc = document.getElementById('tresc');
-    tresc.innerHTML = `
-        <h2>${translations[lang].settingsTitle}</h2>
-        <div class="settings-container">
-            <div class="setting-item">
-                <label for="theme">${translations[lang].themeLabel}</label>
-                <select id="theme">
-                    <option value="light">${translations[lang].lightTheme} <span class="icon-sun">☀️</span></option>
-                    <option value="dark" ${localStorage.getItem('theme') === 'dark' ? 'selected' : ''}>${translations[lang].darkTheme} <span class="icon-moon">🌙</span></option>
-                </select>
-            </div>
-            <div class="setting-item">
-                <label for="language">${translations[lang].languageLabel}</label>
-                <select id="language">
-                    <option value="pl" ${lang === 'pl' ? 'selected' : ''}>${translations[lang].polishLanguage}</option>
-                    <option value="en" ${lang === 'en' ? 'selected' : ''}>${translations[lang].englishLanguage}</option>
-                </select>
-            </div>
-        </div>
-    `;
-}
-
 // Funkcja do renderowania treści w menu
 function renderMenu(lang) {
     const menuItems = document.querySelectorAll('#menu ul li');
+    // Zmiana nazw elementów menu w zależności od języka
     menuItems[0].textContent = translations[lang].homeTitle; // Strona główna
     menuItems[1].textContent = translations[lang].about; // O nas
     menuItems[2].textContent = translations[lang].gallery; // Galeria
