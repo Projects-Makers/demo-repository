6dc78013033fb70768eb85fd2c56b8cb7e389afc
Gniot <orzesekyt@gmail.com>
2024-10-02 10:24:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -96,8 +96,6 @@ function renderSettings(lang) {
 function renderMenu(lang) {
     const menuItems = document.querySelectorAll('#menu ul li');
     menuItems[0].textContent = translations[lang].homeTitle; // Strona główna
-    menuItems[1].textContent = translations[lang].about; // O nas
-    menuItems[2].textContent = translations[lang].gallery; // Galeria
     menuItems[3].textContent = translations[lang].settingsTitle; // Ustawienia
     document.getElementById('login').textContent = translations[lang].login; // Zaloguj się
 }
