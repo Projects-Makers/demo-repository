def77f6839466fcca743671d5dd4ef5a7539cf72
Okruch165 <oskarjablonski272@gmail.com>
2024-10-02 17:15:51 +0200

    Update js_login.js

--- javascript/js_login.js
+++ javascript/js_login.js
@@ -1,7 +1,6 @@
 // Funkcja do ładowania formularza logowania
 function loadLoginForm() {
     const tresc = document.getElementById('tresc');
-    // Renderowanie formularza logowania
     tresc.innerHTML = 
         `<div class="login-container">
             <h2>${translations[currentLanguage].login}</h2>
@@ -12,7 +11,11 @@ function loadLoginForm() {
             </form>
             <p style="text-align: center;"><a href="#" id="registerLink">${translations[currentLanguage].register}</a></p>
         </div>`;
-    // Dodanie nasłuchiwacza do przejścia na formularz rejestracji
     document.getElementById('registerLink').addEventListener('click', loadRegisterForm);
 }
 
+// Nasłuchiwacz kliknięcia w "Zaloguj się"
+document.getElementById('login').addEventListener('click', function(event) {
+    event.preventDefault(); // Zapobiega przeładowaniu strony
+    loadLoginForm(); // Ładowanie formularza logowania
+});
