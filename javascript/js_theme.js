f5e9278e5731a0da2906d3d3d50cb8c897c291ac
Okruch165 <oskarjablonski272@gmail.com>
2024-10-02 17:19:32 +0200

    Update js_theme.js

--- javascript/js_theme.js
+++ javascript/js_theme.js
@@ -1,18 +1,21 @@
-// Funkcja do ustawienia domyślnego motywu
+// Ustawia domyślny motyw na podstawie localStorage
 function setDefaultTheme() {
     const savedTheme = localStorage.getItem('theme') || 'dark';
-    // Ustawienie klasy dla domyślnego motywu
     document.body.classList.add(`${savedTheme}-theme`);
 }
 
-// Funkcja do zmiany motywu
-function changeTheme(theme) {
-    document.body.classList.toggle('light-theme', theme === 'light');
-    document.body.classList.toggle('dark-theme', theme === 'dark');
-    localStorage.setItem('theme', theme); // Zapisanie wybranego motywu w localStorage
-}
-
-// Ustawienie domyślnego motywu przy ładowaniu strony
-document.addEventListener('DOMContentLoaded', function() {
-    setDefaultTheme();
+// Nasłuchuje zmiany motywu i języka
+document.addEventListener('change', function(event) {
+    if (event.target.id === 'theme') {
+        const newTheme = event.target.value;
+        document.body.classList.toggle('light-theme', newTheme === 'light');
+        document.body.classList.toggle('dark-theme', newTheme === 'dark');
+        localStorage.setItem('theme', newTheme); // Zapisuje wybrany motyw
+    } else if (event.target.id === 'language') {
+        currentLanguage = event.target.value;
+        localStorage.setItem('language', currentLanguage); // Zapisuje wybrany język
+        renderHomePage(currentLanguage); // Przeładowuje stronę główną
+        renderSettings(currentLanguage); // Przeładowuje ustawienia
+        renderMenu(currentLanguage); // Przeładowuje menu
+    }
 });
