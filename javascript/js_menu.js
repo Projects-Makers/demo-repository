017f843fcfc30413bb266a3f1322e0ff95f02d83
Okruch165 <oskarjablonski272@gmail.com>
2024-10-02 17:19:12 +0200

    Update js_menu.js

--- javascript/js_menu.js
+++ javascript/js_menu.js
@@ -1,9 +1,10 @@
-// Funkcja do renderowania treści w menu
-function renderMenu(lang) {
-    const menuItems = document.querySelectorAll('#menu ul li');
-    menuItems[0].textContent = translations[lang].homeTitle; // Strona główna
-    menuItems[1].textContent = translations[lang].about; // O nas
-    menuItems[2].textContent = translations[lang].gallery; // Galeria
-    menuItems[3].textContent = translations[lang].settingsTitle; // Ustawienia
-    document.getElementById('login').textContent = translations[lang].login; // Zaloguj się
-}
+// Obsługuje kliknięcia w elementy menu i renderuje odpowiednie treści
+document.getElementById('home').addEventListener('click', function() {
+    renderHomePage(currentLanguage); // Wyświetla stronę główną
+    toggleMenu();  // Zamyka menu
+});
+
+document.getElementById('settings').addEventListener('click', function() {
+    renderSettings(currentLanguage); // Wyświetla ustawienia
+    toggleMenu();  // Zamyka menu
+});
